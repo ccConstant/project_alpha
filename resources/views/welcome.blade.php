db74cfca511a582b690f8998420f06f8ef974106
ccConstant <celine_constant@hotmail.fr>
2022-07-07 08:50:34 +0200

    update the name of the application

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="user-id" content="{{Auth::user()}}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Alpha</title>
+        <title>EQ & MME data mngt SW01</title>
     </head>
     <link href="{{mix('css/app.css')}}" rel="stylesheet">
     <body>
