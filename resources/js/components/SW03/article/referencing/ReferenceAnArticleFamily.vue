f3ccb60e999a92562940d3abd5c55646b21aa83f
ErwanMorand <erwan.morand.em@gmail.com>
2023-05-02 09:16:13 +0200

    Update ReferenceAnArticleFamily.vue

--- resources/js/components/SW03/article/referencing/ReferenceAnArticleFamily.vue
+++ resources/js/components/SW03/article/referencing/ReferenceAnArticleFamily.vue
@@ -23,6 +23,21 @@
                     </div>
                 </div>
             </div>
+            <div class="accordion">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Incoming Inspection
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne">
+                        <div class="accordion-body">
+                            <ReferenceAnIncmgInsp :articleType="this.artFam_type" :article_id="this.artFam_id" :import_id="null"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -30,11 +45,13 @@
 <script>
 import ArticleFamilyForm from "./ArticleFamilyForm";
 import ReferenceAStorageCondition from "./ReferenceAStorageCondition";
+import ReferenceAnIncmgInsp from "../../incInsp/referencing/ReferenceAnIncmgInsp.vue";
 
 
 
 export default {
     components: {
+        ReferenceAnIncmgInsp,
         ArticleFamilyForm,
         ReferenceAStorageCondition,
     },
